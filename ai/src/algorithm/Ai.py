838dbdcef1ffd08f4d1375cd58a353e5b32c0b91
sosantoo <leopold.sallan+gitlab@gmail.com>
2023-05-30 19:31:09 +0200

    [ADD] Base project architecture for AI binary

---
+++ ai/src/algorithm/Ai.py
@@ -0,0 +1,14 @@
+##
+## EPITECH PROJECT, 2023
+## zappy
+## File description:
+## Ai
+##
+
+class Algorithm():
+    def __init__(self):
+        self.inventory = {}
+        self.broadcast_text = []
+        self.command_list = []
+        self.level = 1
+        self.fork = 1
---
+++ ai/src/client/Client.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import socket
+from socket import AF_INET, SOCK_STREAM
+
+BUFFER_SIZE = 4096
+EPITECH_SUCCESS = 0
+EPITECH_ERROR = 84
+
+class Client():
+    def __init__(self, hostname: str, port: str, team_name: str):
+        self.name = team_name
+        self.machine = hostname
+        self.port = port
+        self.socket = None
+
+    def server_connexion(self):
+        try:
+            server_address = (self.machine, self.port)
+            self.socket = socket.socket(AF_INET, SOCK_STREAM)
+            self.socket.connect(server_address)
+            response: str = self.socket.recv(BUFFER_SIZE).decode("utf-8")
+            print(f"{response}", end="")
+            self.socket.sendall((self.name + '\n').encode())
+            response: str = self.socket.recv(BUFFER_SIZE).decode("utf-8")
+            print(f"{response}")
+
+        except ValueError as e:
+            print("Server connexion error:", e)
+
+    def client_launcher(self):
+        try:
+            while True:
+                request: str = input()
+                self.socket.sendall((request + '\n').encode())
+                response: str = self.socket.recv(BUFFER_SIZE).decode("utf-8")
+                print(f"{response}", end="")
+                if "dead" in response:
+                    exit(EPITECH_SUCCESS)
+
+        except KeyboardInterrupt as e:
+            self.socket.close()
+            print("KeyboardInterrupt:", e)
+
+    def close(self):
+        self.socket.close()
