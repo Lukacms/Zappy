2f1dd05f75343e98100c899d17e4273f997d2767
Luka Camus <luka.camus@epitech.eu>
2023-06-21 14:57:09 +0200

    [FIX] server/broadcast => y and x units were (again) switched

--- server/src/server/summons/command_ai/broadcast_func.c
+++ server/src/server/summons/command_ai/broadcast_func.c
@@ -13,7 +13,7 @@
 #include <zappy/server/summon/utils.h>
 #include <zappy/server/utils.h>
 
-static const double DIRECTIONNAL_VECTOR[4] = {0, 90, 180, 270};
+static const double DIRECTIONNAL_VECTOR[4] = {180, 270, 180, 90};
 
 static const double ANGLES[7][3] = {
     {67.5, 112.5, 7},  {112.5, 157.5, 6}, {157.5, 202.5, 5}, {202.5, 247.5, 4},
@@ -26,7 +26,7 @@ static int get_angle(vector2i_t small_vector, client_node_t *client)
         DIRECTIONNAL_VECTOR[client->stats.orientation - 1];
 
     if (my_angle > 360)
-        my_angle -= 360;
+        my_angle = (int)my_angle % 360;
     for (int i = 0; i < 7; i += 1) {
         if (my_angle > ANGLES[i][0] && my_angle < ANGLES[i][1])
             return (int)ANGLES[i][2];
